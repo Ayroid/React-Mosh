3aa314dcdfda4b4379baba78861a1db1c027dcf5
ayroid <ayushsk0000@gmail.com>
2023-07-31 12:04:07 +0530

    form layout ready

---
+++ RCT-Form/src/components/Form/Form.tsx
@@ -0,0 +1,37 @@
+import { FormEvent, useRef } from "react";
+
+const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: " ", age: 0 };
+  const handleSubmit = (e: FormEvent) => {
+    if (nameRef.current && ageRef.current) {
+      person.name = nameRef.current.value;
+      person.age = ageRef.current.valueAsNumber;
+    }
+
+    e.preventDefault();
+    console.log(person);
+  };
+  return (
+    <form action="" onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input ref={nameRef} id="name" type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form">
+          Age
+        </label>
+        <input ref={ageRef} id="age" type="number" className="form-control" />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
