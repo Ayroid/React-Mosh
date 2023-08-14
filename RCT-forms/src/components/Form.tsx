552e71bb92d4471a660712909475600fc3ca2c87
ayroid <ayushsk0000@gmail.com>
2023-08-14 16:48:15 +0530

    useRef hook used

--- RCT-forms/src/components/Form.tsx
+++ RCT-forms/src/components/Form.tsx
@@ -1,12 +1,39 @@
+import { FormEvent, useRef } from "react";
+
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: "", age: 0 };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (nameRef.current !== null) {
+      person.name = nameRef.current.value;
+    }
+    if (ageRef.current !== null) {
+      person.age = parseInt(ageRef.current.value);
+    }
+
+    console.log(person);
+  };
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={nameRef} id="name" type="text" className="form-control" />
       </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input ref={ageRef} id="age" type="number" className="form-control" />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        SUBMIT
+      </button>
     </form>
   );
 };
