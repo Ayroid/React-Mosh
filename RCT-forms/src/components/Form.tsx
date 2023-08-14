23e6756aa66d1d9874f264c470f8a5697d21038c
ayroid <ayushsk0000@gmail.com>
2023-08-14 17:37:54 +0530

    useState hook used to get form fields

--- RCT-forms/src/components/Form.tsx
+++ RCT-forms/src/components/Form.tsx
@@ -1,9 +1,17 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
+  // USING REF HOOK TO GET THE FORM FIELD VALUES
   const nameRef = useRef<HTMLInputElement>(null);
   const ageRef = useRef<HTMLInputElement>(null);
   const person = { name: "", age: 0 };
+
+  // USING STATE HOOK TO GET THE FORM FIELD VALUES
+
+  const [person1, setPerson1] = useState({
+    name: "",
+    age: 0,
+  });
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
@@ -15,6 +23,7 @@ const Form = () => {
     }
 
     console.log(person);
+    console.log(person1);
   };
 
   return (
@@ -23,13 +32,41 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          // USE STATE HOOK
+          onChange={(event) => {
+            setPerson1({
+              ...person1,
+              name: event.target.value,
+            });
+          }}
+          value={person1.name}
+          // USE REF HOOK
+          ref={nameRef}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          // USE STATE HOOK
+          onChange={(event) => {
+            setPerson1({
+              ...person1,
+              age: parseInt(event.target.value),
+            });
+          }}
+          value={person1.age}
+          // USE REF HOOK
+          ref={ageRef}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         SUBMIT
